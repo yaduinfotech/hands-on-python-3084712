21b2f2dd876eb2e74bb172e78a335d2dd1ba5447
yaduinfotech <84190706+yaduinfotech@users.noreply.github.com>
2023-01-17 12:55:30 +0000

    updated

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,9 +4,9 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
+JOHN_PAUL = NAMES[:2] # print first two
+GEORGE_RINGO = NAMES[2:] #print after 2
+REVERSE = NAMES[::-1] # start #end #step
 EVERY_OTHER = NAMES[::2]
 
 print(sum(AGES))
